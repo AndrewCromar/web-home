c8b269b080d486de5978f84622d22f4a0e72161d
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-03-03 11:15:19 -0800

    update email

--- public/pages/qwertyuiopasdfghjklzxcvbnm.php
+++ public/pages/qwertyuiopasdfghjklzxcvbnm.php
@@ -13,9 +13,9 @@ if (!isset($_GET['key']) || $_GET['key'] !== $secret) {
 
 $msg = isset($_GET['msg']) ? urldecode($_GET['msg']) : '(No message provided)';
 
-$subject = "Andrew studied some college stuff!";
+$subject = "Andrew studied some college stuff! (" . date('Y-m-d H:i:s') . ")";
 
-$body = "Hello Scott and Catherine,\n\nAndrew has been studying and he just finished a notable section.\n\nHere is what he worked on:\n" . $msg . "\n\nThanks,\nAutomated Andrew";
+$body = "Hello Scott and Catherine,\n\nAndrew has been studying and he just finished a notable section.\n\nHere is what he worked on:\n\n" . $msg . "\n\nThanks,\nAutomated Andrew";
 
 $to = 'andrewmcromar@gmail.com'; // for testing
 // $to = 'scromar@gmail.com,catcromar@gmail.com';
